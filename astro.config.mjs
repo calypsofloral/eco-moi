28e38b99c5049a15be8abc3a4fd3609583c8bb9f
charlyoleg <charles.braquet@gmx.fr>
2025-09-26 17:11:12 +0200

    base for local

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
+const basePath = process.env.BASE_PATH;
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://calypsofloral.github.io',
-	base: '/eco-moi',
+	//base: '/eco-moi',
+	base: basePath ? basePath : '/',
 	integrations: [mdx(), sitemap()],
 });
